01dca3ae59f44e347308dec47f82483d4a38cf2b
Pietro Paruci <ppmlvd@gmail.com>
2026-04-02 16:12:22 -0300

    Ajustes na tag href e remove configuracao real do firebase do repositorio

--- firebase.js
+++ firebase.js
@@ -1,15 +1,9 @@
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyClyfHrBamNnnT2b9oc74Q62dyd5SlHvuY",
-    authDomain: "adriano-hair-style.firebaseapp.com",
-    projectId: "adriano-hair-style",
-    storageBucket: "adriano-hair-style.firebasestorage.app",
-    messagingSenderId: "272625418330",
-    appId: "1:272625418330:web:a356a9c4abd1ec89f95863",
-    measurementId: "G-Y6DFSTVF79"
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+  appId: "SEU_APP_ID",
+  measurementId: "SEU_MEASUREMENT_ID"
+};

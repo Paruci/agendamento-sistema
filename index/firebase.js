4dcb7afe778b612047446f2bf96808aaba342b4c
Pietro Paruci <ppmlvd@gmail.com>
2026-04-15 13:33:17 -0300

    correcao de dados

--- index/firebase.js
+++ index/firebase.js
@@ -1,16 +1,7 @@
-// Import Firebase via CDN
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_DOMINIO.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_BUCKET.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
 };
 
-// Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Referências úteis
 const auth = firebase.auth();
 const db = firebase.firestore();
